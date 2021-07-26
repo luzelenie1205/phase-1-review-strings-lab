96ceb860780cdfb37f2db7c57f2f29b826e57c71
luzelenie <luzelenie1205@gmail.com>
2021-07-26 14:01:08 -0400

    commit tree 034088b8fc8f47c6bf662b8d5373e56b55015fc9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+console.log( welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+
+const  shortGreeting= `Welcome, ${currentUser[0]}!`
